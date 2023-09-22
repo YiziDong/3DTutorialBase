c57f9eb0fa95827bced0237a1e330ce08b047404
Billy Dong <yizi.666@berkeley.edu>
2023-09-21 19:24:21 -0700

    added new enimies and player skills
    press left shift to use the skill

---
+++ Assets/Scripts/Player/Abilities/ChargeExplore.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChargeExplore : Ability
+{
+
+    public override void use(Vector3 spawnPos)
+    {
+        Collider[] hitColliders = Physics.OverlapBox(gameObject.transform.position, new Vector3(10, 10, 10), Quaternion.identity);
+        foreach (Collider hit in hitColliders)
+        {
+            if (hit.CompareTag("Enemy"))
+            {
+                hit.GetComponent<EnemyController>().DecreaseHealth(m_Info.Power);
+            }
+        }
+        var emitterShape = cc_Ps.shape;
+        emitterShape.length = m_Info.Range;
+        cc_Ps.Play();
+    }
+}
